cd91f3b19aec98727b534172ec7b706d0aa411d7
bryan-m-rojas <bmr333@nyu.edu>
2020-06-12 19:22:53 +0000

    fixed docker prune command to clean up docker env

--- rebuildLaunch.sh
+++ rebuildLaunch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Brings down contatiners
-./develop down && docker system prune -yq
+./develop down && docker system prune -a -f
 
 ## Builds all the docker images from scratch
 ./develop build --no-cache 
