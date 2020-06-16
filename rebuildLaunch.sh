35c834d1c2424a79dc8f370fe817844efe90c7dd
bryan-m-rojas <bmr333@nyu.edu>
2020-06-16 14:40:51 +0000

    added sudo to directory delete to fix permission issue

--- rebuildLaunch.sh
+++ rebuildLaunch.sh
@@ -3,6 +3,16 @@
 ## Brings down contatiners
 ./develop down && docker system prune -a -f
 
+## Check if the specified Docker network exists - if not, create
+NETWORK_NAME=laravel-net
+if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ]; then 
+    docker network create ${NETWORK_NAME};
+    echo "Created ${NETWORK_NAME}"
+else {  
+    echo "${NETWORK_NAME} already exists."
+}
+fi
+
 ## Builds all the docker images from scratch
 ./develop build --no-cache 
 
