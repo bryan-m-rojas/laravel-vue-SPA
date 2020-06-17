5bfebef03a9701e9315c6547ec8d5419c388e42c
bryan-m-rojas <bmr333@nyu.edu>
2020-06-17 18:59:03 +0000

    Refactored tests to make less verbose

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -14,14 +14,7 @@ class ContactsTest extends TestCase
     /** @test */
     public function a_contact_can_be_added()
     {
-        $this->withoutExceptionHandling();
-        
-        $this->post('/api/contacts', [
-            'name' => 'Test Name',
-            'email' => 'test@email.com',
-            'birthday' => '05/14/1988',
-            'company' => 'ABC String',
-        ]);
+        $this->post('/api/contacts', $this->data());
         
         $contact = Contact::first();
         
@@ -31,35 +24,26 @@ class ContactsTest extends TestCase
         $this->assertEquals('ABC String', $contact->company);
     }
     
-    /** @test */
-    public function a_name_is_required()
+     /** @test */
+    public function fields_are_required()
     {
-        $response = $this->post('/api/contacts', [
+        collect(['name', 'email', 'birthday', 'company'])
+            ->each( function ($field) {
+                $response = $this->post('/api/contacts',
+                    array_merge( $this->data(), [$field => '']));
+      
+                $response->assertSessionHasErrors($field);
+                $this->assertCount(0, Contact::all());
+            });
+    }
+    
+    private function data()
+    {
+        return [
+            'name' => 'Test Name',
             'email' => 'test@email.com',
             'birthday' => '05/14/1988',
             'company' => 'ABC String',
-        ]);
-        
-        $contact = Contact::first();
-        
-        $response->assertSessionHasErrors('name');
-        $this->assertCount(0, Contact::all());
-       
-    }
-    
-     /** @test */
-    public function email_is_required()
-    {
-        $response = $this->post('/api/contacts', [
-            'name' => 'Test Name',
-            'birthday' => '05/14/1988',
-            'company' => 'ABC String',
-        ]);
-        
-        $contact = Contact::first();
-        
-        $response->assertSessionHasErrors('email');
-        $this->assertCount(0, Contact::all());
-       
+        ];
     }
 }
