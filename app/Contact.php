829890466d428b48379a91ad519a6d029597d733
bryan-m-rojas <bmr333@nyu.edu>
2020-06-17 19:28:00 +0000

    Added test to validate emails and check if birthdays are properly stored

--- app/Contact.php
+++ app/Contact.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     protected $guarded = [];
+    
+    protected $dates = ["birthday"];
+    
+    public function setBirthdayAttributes($birthday)
+    {
+        $this->attributes['birthday'] = carbon::parse($birthday);
+    }
 }
--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Carbon\Carbon;
 use App\Contact;
 use Tests\TestCase;
 
@@ -31,10 +32,33 @@ class ContactsTest extends TestCase
             ->each( function ($field) {
                 $response = $this->post('/api/contacts',
                     array_merge( $this->data(), [$field => '']));
-      
+                
                 $response->assertSessionHasErrors($field);
                 $this->assertCount(0, Contact::all());
             });
+    }
+    
+    /** @test */
+    public function email_must_be_a_valid_email()
+    {
+        $response = $this->post('/api/contacts',
+            array_merge( $this->data(), ['email' => 'NOT AN EMAIL']));
+          
+        $response->assertSessionHasErrors('email');
+        $this->assertCount(0, Contact::all());   
+    }
+    
+     /** @test */
+    public function birthdays_are_properly_stored()
+    {
+        $this->withoutExceptionHandling();
+        
+        $response = $this->post('/api/contacts',
+            array_merge( $this->data()));
+          
+        $this->assertCount(1, Contact::all());   
+        $this->assertInstanceOf( Carbon::class, Contact::first()->birthday);
+        $this->assertEquals('05-14-1988', Contact::first()->birthday->format('m-d-Y'));
     }
     
     private function data()
