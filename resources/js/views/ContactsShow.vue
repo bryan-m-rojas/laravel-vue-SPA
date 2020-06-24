e2e130fdfb1115143c591bd83613c0adb60e2878
bryan-m-rojas <bmr333@nyu.edu>
2020-06-24 16:00:47 +0000

    Implemented Contacts Show view

--- resources/js/views/ContactsShow.vue
+++ resources/js/views/ContactsShow.vue
@@ -1,5 +1,30 @@
 <template>
     <div>
+        <div class="flex justify-between">
+            <div class="text-blue-400">
+                < Back 
+            </div>
+            
+            <div>
+                <router-link :to="'/contacts' + contact.id + '/edit'" class="px-4 py-2 rounded text-sm font-bold text-green-500 border border-green-500 mr-2">Edit</router-link>
+                <a href="#" class="px-4 py-2 border border-red-500 rounded text-sm font-bold text-red-500">Delete</a>
+            </div>
+        </div>
+        
+        <div class="flex items-center pt-6">
+            <div>VG</div>
+            
+            <p class="pl-5 text-xl">{{ contact.name }}</p>
+        </div>
+        
+        <p class="pt-6 text-gray-600 font-bold uppercase text-sm">Contact</p>
+        <p class="pt-2 text-blue-400">{{ contact.email }}</p>
+        
+        <p class="pt-6 text-gray-600 font-bold uppercase text-sm">Company</p>
+        <p class="pt-2 text-blue-400">{{ contact.company }}</p>
+        
+        <p class="pt-6 text-gray-600 font-bold uppercase text-sm">Birthday</p>
+        <p class="pt-2 text-blue-400">{{ contact.birthday }}</p>
         
     </div>
 </template>
