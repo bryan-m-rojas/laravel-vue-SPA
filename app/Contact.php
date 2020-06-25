45dac0a18187b9ba93c15e029fe1cb56e227fbc9
bryan-m-rojas <bmr333@nyu.edu>
2020-06-25 15:26:59 +0000

    Created test for fetching Birthdays

--- app/Contact.php
+++ app/Contact.php
@@ -15,6 +15,11 @@ class Contact extends Model
         return '/contacts/' . $this->id;        
     }
     
+    public function scopeBirthdays($query)
+    {
+        return $query->whereRaw('birthday like "%-' . now()->format('m') . '-%"');
+    }
+    
     public function setBirthdayAttributes($birthday)
     {
         $this->attributes['birthday'] = Carbon::parse($birthday);
