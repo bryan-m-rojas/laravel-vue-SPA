24a4ef8771e5e8be1a346337c6154876b8223f63
bryan-m-rojas <bmr333@nyu.edu>
2020-06-25 16:54:38 +0000

    Installed and setup Laravel Scout for search

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Resources\Contact as ContactResource;
+
+class SearchController extends Controller
+{
+    public function index()
+    {
+        $data = request()->validate([
+            'searchTerm' => 'required',
+        ]);
+        
+        $contacts = Contact::search($data['searchTerm'])->get();
+        
+        return ContactResource::collection($contacts);
+    }
+}
